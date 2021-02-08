1833e623cd64e17e94257cb8c51b3a8720ee8ac8
yannrdg <yann.radigon@gmail.com>
2021-02-08 15:23:50 +0100

    Création de la base du jeu
    J'ai tout d'abord créé plusieurs constante qui permettent de récupérer la grille ainsi que les croix & les ronds.
    J'ai ensuite 2 tableaux qui correspondent aux cases de la grille
    - 1 qui permet de savoir si la case a été cochée (tDiv)
    - 1 qui permet de savoir qui a joué (tResult)
    
    J'ai créé une fonction cocherCase qui permet de savoir qui a coché la case et de ne pas pouvoir la recocher ou la décocher.
    Une autre fonction compteur(), qui permet de savoir quel joeur doit jouer.
    Une fonction rejouer() qui permet de rejouer lorsque la partie est terminée.
    Une fonction gagner qui récupère toutes les possibilités et de déterminer qui a gagné.
    
    Toutes ces fonctions sont appelées lorsqu'une case est cochée.
    
    Au début de la partie, le nom de chaque joueur est demandé grâce à la fonction prompt() stockée dans 2 constantes relatives aux 2 joueurs.

---
+++ script.js
@@ -0,0 +1,193 @@
+const divCase = document.querySelectorAll("#grid>div");
+const button = document.querySelector("#rejouer");
+const rond = document.getElementsByClassName("rond");
+const croix = document.getElementsByClassName("croix");
+const gagnant = document.querySelector("#gagnant");
+const divCroix = document.querySelectorAll(".croix>div");
+//let color1;
+//let color2;
+
+button.style.display = "none";
+
+button.addEventListener("click", () => {
+    window.location.reload();
+})
+
+let compteurDeTour = 0;
+
+let play = false;
+
+let tDiv = [
+	false, false, false, 
+	false, false, false, 
+	false, false, false
+];
+
+let tResult = [
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0
+]
+
+function cocherCase(i) {
+    if(compteurDeTour%2 == 0)
+    {
+        rond[i].style.display = "block";
+        tResult[i] = 1;
+    }
+    else
+    {
+        croix[i].style.display = "block";
+        tResult[i] = 2;
+    }
+}
+
+function compteur(){
+    if(compteurDeTour < 9)
+    {
+        compteurDeTour +=1;
+    }
+    else
+    {
+        compteurDeTour = 0;
+    }
+}
+
+function rejouer()
+{
+    if(play == true)
+    {
+        for(let j = 0; j < 9; j += 1)
+        {
+            tDiv[j] = true;
+        }
+        button.style.display = "block";
+    }
+}
+
+function gagner(){
+    //Pour les ronds Joueur 2
+    if(tResult[0] == 1 && tResult[1] == 1 && tResult[2] == 1){
+        //Horizontal haut
+        play = true;
+        gagnant.innerText = j2 + " a gagné !";
+    }else if(tResult[0] == 1 && tResult[3] == 1 && tResult[6] == 1)
+    {
+        //Verticale gauche
+        play = true;
+        gagnant.innerText = j2 + " a gagné !";
+    }else if(tResult[0] == 1 && tResult[4] == 1 && tResult[8] == 1)
+    {
+        //Diagonale HG/BD
+        play = true;
+        gagnant.innerText = j2 + " a gagné !";
+    }else if(tResult[2] == 1 && tResult[4] == 1 && tResult[6] == 1)
+    {
+        //Diagonale HD/BG
+        play = true;
+        gagnant.innerText = j2 + " a gagné !";
+    }else if(tResult[2] == 1 && tResult[5] == 1 && tResult[8] == 1)
+    {
+        //Verticale droite
+        play = true;
+        gagnant.innerText = j2 + " a gagné !";
+    }else if(tResult[1] == 1 && tResult[4] == 1 && tResult[7] == 1)
+    {
+        //Verticale milieu
+        play = true;
+        gagnant.innerText = j2 + " a gagné !";
+    }else if(tResult[3] == 1 && tResult[4] == 1 && tResult[5] == 1)
+    {
+        //horizontale centre
+        play = true;
+        gagnant.innerText = j2 + " a gagné !";
+    }else if(tResult[6] == 1 && tResult[7] == 1 && tResult[8] == 1)
+    {
+        //horizontale bas
+        play = true;
+        gagnant.innerText = j2 + " a gagné !";
+    }
+    //Pour les croix Joueur 1
+    else if (tResult[0] == 2 && tResult[1] == 2 && tResult[2] == 2){
+        //Horizontal haut
+        play = true;
+        gagnant.innerText = j1 + " a gagné !";
+    }
+    else if(tResult[0] == 2 && tResult[3] == 2 && tResult[6] == 2)
+    {
+        //Verticale gauche
+        play = true;
+        gagnant.innerText = j1 + " a gagné !";
+    }else if(tResult[0] == 2 && tResult[4] == 2 && tResult[8] == 2)
+    {
+        //Diagonale HG/BD
+        play = true;
+        gagnant.innerText = j1 + " a gagné !";
+    }else if(tResult[2] == 2 && tResult[4] == 2 && tResult[6] == 2)
+    {
+        //Diagonale HD/BG
+        play = true;
+        gagnant.innerText = j1 + " a gagné !";
+    }else if(tResult[2] == 2 && tResult[5] == 2 && tResult[8] == 2)
+    {
+        //Verticale droite
+        play = true;
+        gagnant.innerText = j1 + " a gagné !";
+    }else if(tResult[1] == 2 && tResult[4] == 2 && tResult[7] == 2)
+    {
+        //Verticale milieu
+        play = true;
+        gagnant.innerText = j1 + " a gagné !";
+    }else if(tResult[3] == 2 && tResult[4] == 2 && tResult[5] == 2)
+    {
+        //horizontale centre
+        play = true;
+        gagnant.innerText = j1 + " a gagné !";
+    }else if(tResult[6] == 2 && tResult[7] == 2 && tResult[8] == 2)
+    {
+        //horizontale bas
+        play = true;
+        gagnant.innerText = j1 + " a gagné !";
+    }
+}
+
+
+
+
+const j1 = prompt("Entrez votre prénom du joueur 1","");
+//const colorJ1 = prompt("Quelle est votre couleur préférée joueur 1","");
+const j2 = prompt("Entrez votre prénom du joueur 2","");
+//const colorJ2 = prompt("Quelle est votre couleur préférée joueur 2","");
+
+
+/*for(r = 0; r < 9; r += 1){
+    rond[r].style.background = color2;
+}
+
+for(c= 0; c < 18; c += 1){
+    divCroix[c].style.background = color1;
+}*/
+
+if(j1 == (null || ""))
+{
+    window.location.reload();
+}else if(j2 == (null || "")){
+    window.location.reload();
+}
+
+for(let i = 0; i < 9; i += 1)
+{
+    divCase[i].addEventListener("click", () => {
+        if(tDiv[i] === false)
+        {
+            compteur();
+            cocherCase(i);
+            console.log(compteurDeTour);
+            tDiv[i] = true;
+            gagner();
+            rejouer();
+            console.log(tResult);
+            console.log(tDiv);
+        }
+    });
+}
